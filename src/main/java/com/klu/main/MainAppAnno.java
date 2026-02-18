26c369748d7d2f63fbe4c3f0296764cd9543de30
Gnanasree-2400032296 <2400032296@kluniversity.in>
2026-02-18 10:01:00 +0530

    Skill4 workbook project

---
+++ src/main/java/com/klu/main/MainAppAnno.java
@@ -0,0 +1,18 @@
+package com.klu.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.klu.config.AppConfig;
+import com.klu.model.StudentAnno;
+
+public class MainAppAnno {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        StudentAnno student = context.getBean(StudentAnno.class);
+        student.display();
+    }
+}
